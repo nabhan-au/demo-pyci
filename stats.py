b0882bb2bd46d30ccdd38534a28c2a148718d3f7
nabhan <nabhan.au@gmail.com>
2021-10-04 14:44:23 +0700

    fix stat and readme

--- stats.py
+++ stats.py
@@ -30,7 +30,7 @@ def variance(data):
     # some deliberately misformatted code. Use flake8 to fix.
     n=len(data)  
     if n==0:
-        raise TypeError( )
+        raise ValueError("value should not be zero")
     average = sum(data)/n
     return sum( [(x-average)**2 for x in data] )/n
 
